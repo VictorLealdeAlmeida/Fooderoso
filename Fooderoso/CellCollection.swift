26b81a3178e9e3f07fad7e7b8189b4d02ee37c70
VictorLealdeAlmeida <VictorLeal@MacBook-Pro-de-Victor-2.local>
2016-11-03 16:13:51 -0300

    first commit

---
+++ Fooderoso/CellCollection.swift
@@ -0,0 +1,9 @@
+//
+//  CellCollection.swift
+//  Fooderoso
+//
+//  Created by Victor Leal Porto de Almeida Arruda on 03/11/16.
+//  Copyright © 2016 AlunosDeKiev. All rights reserved.
+//
+
+import Foundation
